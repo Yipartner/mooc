5d1a1553e5ef4931a0a43e8859614b03cfee30ba
hanxiao <1350690718@qq.com>
2018-01-27 19:02:55 +0800

    文件链接修改

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -21,7 +21,7 @@ class FileUploadController extends Controller
         $saveMp4Entry=base64_urlSafeEncode($this->bucket.":test1.mp4");
         //视频处理方式
         $url=base64_urlSafeEncode("p37gfblil.bkt.clouddn.com");
-        $videoDeal ="avthumb/m3u8/domain/".$url."/vb/500k|saveas/".$saveMp4Entry;
+        $videoDeal ="avthumb/m3u8/noDomain/0/vb/500k|saveas/".$saveMp4Entry;
         $policy = array(
             'persistentOps' => $videoDeal,
             'persistentPipeline' => "video-pipe",
